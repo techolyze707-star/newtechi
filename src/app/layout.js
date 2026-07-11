f03472f3bf2ee9a4b35673b2d0f8498dc62ae1b4
usmandev918 <usman918khan@gmail.com>
2026-07-11 18:49:14 +0500

    fd

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-import LayoutContent from "@/components/LayoutContent";
+import LayoutContent from "@/components/Layout  Content";
 import { generateDocumentMetadata, generateOrganizationStructuredData } from "@/lib/seo-utils";
 // Headers import removed to allow static generation
 
@@ -53,21 +53,7 @@ export default function RootLayout({ children }) {
     <html lang="en" data-scroll-behavior="smooth">
       <head>
         {/* Google Tag Manager */}
-        {/* <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];
-            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-            var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-            j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-            f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MFDXN6QK');
-          `,
-          }}
-        /> */}
+
         {/* End Google Tag Manager */}
 
         {/* Organization Structured Data */}
