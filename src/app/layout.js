52ae829df2ecb36ad331a61446a10db7636dfa3b
usmandev918 <usman918khan@gmail.com>
2026-07-23 10:09:08 +0500

    grow_added

--- src/app/layout.js
+++ src/app/layout.js
@@ -79,6 +79,10 @@ export default function RootLayout({ children }) {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="msapplication-TileColor" content="#0a0a0a" />
+
+        <script data-grow-initializer="">
+          {`!(function(){window.growMe||((window.growMe=function(e){window.growMe.push(e);}),(window.growMe=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZTo1Y2I4NGY4OC1jNGVjLTQ0ZDMtOGIwZS1mYzRlYTE3ZDg5YmQ=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();`}
+        </script>
       </head>
       <body className="antialiased min-h-screen flex flex-col bg-gray-50">
         {/* Google Analytics */}
